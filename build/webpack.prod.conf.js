9ae69cb30c3d5c982ae030767d2b8a1190fb9e70
careycui <cuihaiyu@hikvision.com>
2017-08-15 18:32:02 +0800

    c

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -48,24 +48,6 @@ var webpackConfig = merge(baseWebpackConfig, {
         safe: true
       }
     }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    // new HtmlWebpackPlugin({
-    //   filename: config.build.index,
-    //   template: 'index.html',
-    //   inject: true,
-    //   minify: {
-    //     removeComments: true,
-    //     collapseWhitespace: true,
-    //     removeAttributeQuotes: true
-    //     // more options:
-    //     // https://github.com/kangax/html-minifier#options-quick-reference
-    //   },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      // chunksSortMode: 'dependency'
-    // }),
-    // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks: function (module, count) {
@@ -93,7 +75,6 @@ var webpackConfig = merge(baseWebpackConfig, {
         ignore: ['.*']
       }
     ]),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new ExtractTextPlugin(path.join(config.build.assetsSubDirectory, '[name].[contenthash].css'))
   ]
 })
