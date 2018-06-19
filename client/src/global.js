9d41b518f8872fa02734754cea47a6b1f5c7fc04
careycui <haiyucui@gmail.com>
2018-06-19 19:03:59 +0800

    add
    config change commit

--- client/src/global.js
+++ client/src/global.js
@@ -1,11 +1,11 @@
 window.G = {};
 window.G.API = {
 	// host: '//172.7.14.58:3030/'
-	host: '//localhost:3030/'
+	host: '//10.12.37.110:3030/'
 }
 window.G.STATIC = {
 	// host: '//172.7.14.58:8080/'
-	host: '//localhost:8080/'
+	host: '//10.12.37.110:8080/'
 }
 window.G.M = {
 	clientWidth: 375
